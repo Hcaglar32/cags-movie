1638afb9e1949ff6f37b3a7679d9129a80298794
Hasan Emir Cağlar <hasan.49.50@hotmail.com>
2024-03-08 17:28:42 +0300

    Veriler api'den çekildi ve listelendi.

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,58 @@
 import React, { Component, useEffect, useState } from "react";
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
-import { data } from "autoprefixer";
 import axios from "axios";
+
+
 const App = () => {
   const [movies, setMovies] = useState([]);
 
 
-
-  /* useEffect(() => {
-     const fetchData = async () => {
-       const baseUrl = "http://localhost:3000/movies";
-       const response = await fetch(baseUrl);
-       const data = await response.json();
-       console.log(response);
-       console.log(data);
-       setMovies(data); 
-     };
-   
-     fetchData();
-   }, []);*/
+  
 
   useEffect(() => {
     const axiosData = async () => {
-      const response = await axios.get('http://localhost:3000/movies');
-      setMovies(response.data)
-      //console.log(response);
+      let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200&api_key=01a73c2decaf92451e35489578627fc3',
+        headers: {
+          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMWE3M2MyZGVjYWY5MjQ1MWUzNTQ4OTU3ODYyN2ZjMyIsInN1YiI6IjY1ZWFmNWIyOTRkOGE4MDE3YjhmYTlmOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-2a79VjLsPayZuTleeZ0NUjXtWq3JLMIxRi58LoYUNA',
+          'accept': 'application/json'
+        }
+      };
+
+      try {
+        const response = await axios(config);
+        return response.data;
+      } catch (error) {
+        console.error('Hata oluştu:', error);
+        throw error; // Hata durumunda hatayı yeniden fırlatın
+      }
     };
+
+    axiosData()
+      .then(data => {
+        setMovies(data.results)
+        console.log(data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+
     axiosData();
   }, []);
 
+
+
+
   const [searchQuery, setSearchQuery] = useState("");
-  
 
-  /* FETCH  
-  const deleteMovie = async (movie) => {
-    const baseUrl = await `http://localhost:3000/movies/${movie.id}`;
-    await fetch(baseUrl, {
-      method: "DELETE",
-    });
 
-    const newMovieList = movies.filter(m => m.id !== movie.id);
-    setMovies(newMovieList);
-  };*/
+
 
   const deleteMovie = async (movie) => {
-     axios.delete(`http://localhost:3000/movies/${movie.id}`);
+    axios.delete(`http://localhost:3000/movies/${movie.id}`);
     const newMovieList = movies.filter(m => m.id !== movie.id);
     setMovies(newMovieList);
   };
@@ -54,9 +61,11 @@ const App = () => {
     setSearchQuery(event.target.value);
   }
 
+
   let filteredMovies = movies.filter((movie) => {
-    return movie.name.toLowerCase().indexOf(searchQuery.toLowerCase()) !== -1;
+    return movie.title.toLowerCase().indexOf(searchQuery.toLowerCase()) !== -1;
   });
+
 
   return (
     <div className="container-fluid mx-auto">
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -11,12 +11,12 @@ const MovieList = ({ movies, deleteMovieProp }) => {
                     <div className="movie-card mb-8 w-1/2 flex" key={movie.id}>
                         <div className="flex items-center mr-8 space-x-4 bg-white rounded-lg shadow-md p-5">
                             <div className="img-area w-1/4">
-                                <img src={movie.imageURL} alt="Film Poster" className="w-40 ml-2 h-auto rounded-lg" />
+                                <img src={`https://media.themoviedb.org/t/p/w220_and_h330_face/${movie.poster_path}`} alt="Film Poster" className="w-40 ml-2 h-auto rounded-lg" />
                             </div>
                             <div className="flex w-1/2 flex-col">
                                 <div className="contents">
-                                    <h2 className="text-lg font-semibold">{movie.name}</h2>
-                                    <p className="text-gray-500">IMDB: {movie.rating}</p>
+                                    <h2 className="text-lg font-semibold">{movie.title}</h2>
+                                    <p className="text-gray-500">IMDB: {movie.vote_average}</p>
                                     <p>{movie.overview}</p>
                                 </div>
                                 <button onClick={event => deleteMovieProp(movie)} className=' flex justify-center mt-5 p-2 bg-red-600 text-white rounded-lg border-2 w-1/2 text-center'>Sil</button>
